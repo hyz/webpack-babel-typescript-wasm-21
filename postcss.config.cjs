c43f88dbbe981c243434358e9d1cc08f44247276
oops <cooloops@qq.com>
2021-10-21 11:54:14 +0800

    postcss

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,21 +1,24 @@
+/* eslint-disable node/no-unpublished-require */
 const tailwindcss = require('tailwindcss');
 const autoprefixer = require('autoprefixer');
-
-module.exports = /*function (api)*/ {
-  plugins: [
-    tailwindcss,
-    autoprefixer,
-    'postcss-import',
-    'postcss-nested',
-    'postcss-preset-env',
-    // ['postcss-pxtorem': {
-    //   rootValue: 16,
-    //   unitPrecision: 5,
-    //   propList: ['*'],
-    //   selectorBlackList: ['html', 'body'],
-    //   replace: true,
-    //   mediaQuery: false,
-    //   minPixelValue: 0,
-    // }],
-  ],
+module.exports = api => {
+  return {
+    plugins: [
+      [tailwindcss, {}],
+      [autoprefixer, {}],
+      ['postcss-import', {}],
+      // 'postcss-nested': {},
+      ['postcss-preset-env', {}],
+      // 'postcss-pxtorem': {
+      //   rootValue: 16,
+      //   unitPrecision: 5,
+      //   propList: ['*'],
+      //   selectorBlackList: ['html', 'body'],
+      //   replace: true,
+      //   mediaQuery: false,
+      //   minPixelValue: 0,
+      // },
+    ],
+  };
 };
+
